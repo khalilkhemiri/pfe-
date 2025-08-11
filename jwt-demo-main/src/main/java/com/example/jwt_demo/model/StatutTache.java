e90bb554f49fecf2eb3ea441e567cd2356cae17f
khalilkhemiri <126549623+khalilkhemiri@users.noreply.github.com>
2025-08-11 03:17:19 +0200

    Delete jwt-demo-main directory

--- jwt-demo-main/src/main/java/com/example/jwt_demo/model/StatutTache.java
+++
@@ -1,7 +0,0 @@
-package com.example.jwt_demo.model;
-
-public enum StatutTache {
-    EN_ATTENTE,
-    EN_COURS,
-    TERMINEE
-}
--- jwt-demo-main/src/main/java/com/example/jwt_demo/model/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.example.jwt_demo.model;
-
-public enum UserRole {
-    STAGIAIRE, TUTEUR, ADMIN
-
-}
